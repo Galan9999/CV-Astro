6d6e74c6e2d1c8312b753b4f70e37412cf6439c6
cgala <carles@legalpigeon.com>
2026-05-13 16:15:34 +0200

    chore: site URL + path aliases — @components @layouts @data @styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://galan9999.github.io/CV-Astro',
   output: 'static',
   integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '@components': '/src/components',
+        '@layouts':    '/src/layouts',
+        '@data':       '/src/data',
+        '@styles':     '/src/styles',
+      }
+    }
+  }
 });
